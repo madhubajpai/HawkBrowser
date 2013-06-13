9ae3577741c88f7b65bc152a4da80284eeee9f69
chenshanglin <chenshanglin@gmail.com>
2013-06-13 19:58:06 +0800

    test

--- src/com/example/hawkbrowser/core/HawkWebView.java
+++ src/com/example/hawkbrowser/core/HawkWebView.java
@@ -24,6 +24,8 @@ public class HawkWebView extends WebView {
 		Init();
 	}
 
+	
+	
 	public HawkWebView(Context context, AttributeSet attrs, int defStyle) {
 		super(context, attrs, defStyle);
 		// TODO Auto-generated constructor stub
